a2d71eee5b93bdabca6770773d21e15ae8b44639
ishitachitranshi <168059493+ishitachitranshi@users.noreply.github.com>
2025-12-16 19:34:34 +0530

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -6,13 +6,16 @@ from recommender import recommend
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+# Templates folder
 templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/", response_class=HTMLResponse)
-def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "results": []})
 
-@app.get("/recommend")
-def get_recommendation(query: str):
-    return {"results": recommend(query)}
+@app.post("/search", response_class=HTMLResponse)
+async def search_keyword(request: Request, keyword: str = Form(...)):
+    results = recommend(keyword, top_n=5)
+    return templates.TemplateResponse("index.html", {"request": request, "results": results, "keyword": keyword})
+
